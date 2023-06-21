7292fbb82ea4a1b0c671fb5768d89d258f55f72a
bharathidevarasu <bharathi@whatscookn.com>
2023-06-21 08:12:42 +0400

    new changes are added.

--- next.config.js
+++ next.config.js
@@ -1,17 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   reactStrictMode: true,
-  swcMinify: true,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    domains: ["lh3.googleusercontent.com", "vercel.com"],
-  },
- 
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
