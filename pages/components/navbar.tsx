c92799e6b2470a0c16c2fc5f912d197b66de6ccc
bharathidevarasu <bharathi@whatscookn.com>
2023-06-27 12:03:23 +0400

    chakra ui changes are added.

--- pages/components/navbar.tsx
+++ pages/components/navbar.tsx
@@ -1,107 +1,109 @@
-import React, { useState } from 'react';
+import { Fragment, useState } from 'react'
+import { Disclosure, Menu, Transition } from '@headlessui/react'
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 
-const Navbar = () => {
-  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+const navigation = [
+  { name: 'Dashboard', href: '#', current: true },
+  { name: 'Team', href: '#', current: false },
+  { name: 'Projects', href: '#', current: false },
+  { name: 'Calendar', href: '#', current: false },
+]
 
-  const handleItemClick = () => {
-    // Handle item click logic here
-  };
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
 
-  const handleToggleMenu = () => {
-    setMobileMenuOpen(!isMobileMenuOpen);
-  };
+export default function Navbar() {
+
+  const [open, setOpen]= useState(false);
 
   return (
-    <div className="bg-white">
-      <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
-        <div className="flex lg:flex-1">
-          <a href="#" className="-m-1.5 p-1.5">
-            <span className="sr-only">Your Company</span>
-            <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
-          </a>
-        </div>
-        <div className="flex lg:hidden">
-          <button
-            type="button"
-            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-            onClick={handleToggleMenu}
-            aria-label={isMobileMenuOpen ? 'Close menu' : 'Open main menu'}
-          >
-            <span className="sr-only">{isMobileMenuOpen ? 'Close menu' : 'Open main menu'}</span>
-            {isMobileMenuOpen ? (
-              <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-              </svg>
-            ) : (
-              <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
-              </svg>
-            )}
-          </button>
-        </div>
-        <div className={`lg:hidden ${isMobileMenuOpen ? 'flex' : 'hidden'}`}>
-          <div className="fixed inset-0 z-10"></div>
-          <div className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-            <div className="flex items-center justify-between">
-              <a href="#" className="-m-1.5 p-1.5">
-                <span className="sr-only">Your Company</span>
-                <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
-              </a>
-              <button
-                type="button"
-                className="-m-2.5 rounded-md p-2.5 text-gray-700"
-                onClick={handleToggleMenu}
-                aria-label="Close menu"
-              >
-                <span className="sr-only">Close menu</span>
-                <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-                </svg>
-              </button>
-            </div>
-            <div className="mt-6">
-              <a href="#" className="block rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50" onClick={handleItemClick}>
-                Product
-              </a>
-              <a href="#" className="block rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50" onClick={handleItemClick}>
-                Features
-              </a>
-              <a href="#" className="block rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50" onClick={handleItemClick}>
-                Marketplace
-              </a>
-              <a href="#" className="block rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50" onClick={handleItemClick}>
-                Company
-              </a>
-            </div>
-            <div className="py-6">
-              <a href="#" className="block rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50" onClick={handleItemClick}>
-                Log in
-              </a>
+    <Disclosure as="nav" className="bg-gray-800">
+        <>
+
+          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+            <div className="relative flex h-16 items-center justify-between">
+              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+
+
+                {/* Mobile menu button*/}
+                <Disclosure.Button  className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+              </div>
+
+            
+              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                
+              <div className="hidden sm:ml-6 sm:block">
+                  <div className="flex space-x-4">
+
+                    <a
+                      key={'categories'}
+                      href={'/categories'}
+                      className={classNames('text-gray-300 hover:bg-gray-700 hover:text-white',
+                        'rounded-md px-3 py-2 text-sm font-medium'
+                      )}
+                      aria-current={'page'}
+                    >
+                      {'AIToolsNext'}
+                    </a>
+
+                  </div>
+                </div>
+
+                <div className="hidden sm:ml-6 sm:block">
+                  <div className="flex space-x-4">
+
+                    <a
+                      key={'categories'}
+                      href={'/categories'}
+                      className={classNames('text-gray-300 hover:bg-gray-700 hover:text-white',
+                        'rounded-md px-3 py-2 text-sm font-medium'
+                      )}
+                      aria-current={'page'}
+                    >
+                      {'Categories'}
+                    </a>
+
+                  </div>
+                </div>
+
+              </div>
+              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                <button
+                  type="button"
+                  className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                >
+                  Get Deals
+                </button>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="hidden lg:flex lg:gap-x-12">
-          <a href="#" className="text-sm font-semibold leading-6 text-gray-900 hover:text-blue-500" onClick={handleItemClick}>
-            Product
-          </a>
-          <a href="#" className="text-sm font-semibold leading-6 text-gray-900 hover:text-blue-500" onClick={handleItemClick}>
-            Features
-          </a>
-          <a href="#" className="text-sm font-semibold leading-6 text-gray-900 hover:text-blue-500" onClick={handleItemClick}>
-            Marketplace
-          </a>
-          <a href="#" className="text-sm font-semibold leading-6 text-gray-900 hover:text-blue-500" onClick={handleItemClick}>
-            Company
-          </a>
-        </div>
-        <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-          <a href="#" className="text-sm font-semibold leading-6 text-gray-900 hover:text-blue-500">
-            Log in <span aria-hidden="true">&rarr;</span>
-          </a>
-        </div>
-      </nav>
-    </div>
-  );
-};
 
-export default Navbar;
+          <Disclosure.Panel className="sm:hidden">
+            <div className="space-y-1 px-2 pb-3 pt-2">
+                <Disclosure.Button
+                  key={'Categories'}
+                  as="a"
+                  href={'/categories'}
+                  className={classNames('text-gray-300 hover:bg-gray-700 hover:text-white',
+                    'block rounded-md px-3 py-2 text-base font-medium'
+                  )}
+                  aria-current={'page'}
+                >
+                  {'Categories'}
+                </Disclosure.Button>
+            </div>
+          </Disclosure.Panel>
+
+        </>
+    
+    </Disclosure>
+  )
+}
